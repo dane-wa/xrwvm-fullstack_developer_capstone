74d03297f2c9d2e42a4f5b71528ecaf7b4658498
Dane <camaradane2000@hotmail.fr>
2026-02-16 19:30:26 +0000

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -22,7 +22,7 @@ logger = logging.getLogger(__name__)
 @csrf_exempt
 def login_user(request):
     # Crée une vue login_request pour gérer les requêtes de connexion.
-    # Récupérer le nom d’utilisateur et 
+    # Récupérer le nom d’utilisateur et
     # le mot de passe depuis le dictionnaire request.POST
     data = json.loads(request.body)
     username = data['userName']
@@ -32,7 +32,7 @@ def login_user(request):
     user = authenticate(username=username, password=password)
     data = {"userName": username}
     if user is not None:
-        # Si l’utilisateur est valide, appeler 
+        # Si l’utilisateur est valide, appeler
         # la méthode login pour connecter l’utilisateur courant.
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
@@ -42,7 +42,7 @@ def login_user(request):
 def logout_request(request):
     # Créer une vue logout_request pour gérer la demande de déconnexion.
     logout(request) # Mettre fin à la session de l’utilisateur
-    data = {"userName":""} # Retourner un nom d’utilisateur vide
+    data = {"userName": ""} # Retourner un nom d’utilisateur vide
     return JsonResponse(data)
 
 
@@ -78,10 +78,10 @@ def registration(request):
         )
         # Connecter l’utilisateur et le rediriger vers la page de liste.
         login(request, user)
-        data = {"userName":username,"status":"Authenticated"}
+        data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
     else:
-        data = {"userName":username,"error":"Already Registered"}
+        data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
 
@@ -93,15 +93,20 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append(
+            {
+                "CarModel": car_model.name,
+                "CarMake": car_model.car_make.name,
+            },
+        )
     return JsonResponse({"CarModels":cars})
 
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-#Update the `get_dealerships` render list
-# of dealerships all by default, particular state if state is passed
+
 def get_dealerships(request, state="All"):
+    a list of dealerships
+    # def get_dealerships(request):
+    # update the `get_dealerships` render list
+    # of dealerships all by default, particular state if state is passed
     if(state == "All"):
         endpoint = "/fetchDealers"
     else:
@@ -109,9 +114,10 @@ def get_dealerships(request, state="All"):
     dealerships = get_request(endpoint)
     return JsonResponse({"status":200,"dealers":dealerships})
 
-# Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
+
 def get_dealer_reviews(request, dealer_id):
+    # Create a `get_dealer_reviews` view to render the reviews of a dealer
+    # def get_dealer_reviews(request,dealer_id):
     # if dealer id has been provided
     if(dealer_id):
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
@@ -124,9 +130,10 @@ def get_dealer_reviews(request, dealer_id):
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
 
-# Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
+
 def get_dealer_details(request, dealer_id):
+    # Create a `get_dealer_details` view to render the dealer details
+    # def get_dealer_details(request, dealer_id):
     if (dealer_id):
         endpoint = "/fetchDealer/"+str(dealer_id)
         dealership = get_request(endpoint)
