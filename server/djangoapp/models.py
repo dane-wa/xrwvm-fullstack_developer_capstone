01f000653c79c9fa955041f89f14bd65340540dc
Dane <camaradane2000@hotmail.fr>
2026-02-16 11:55:38 +0000

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,4 +1,3 @@
-# Uncomment the following imports before adding the Model code
 
 from django.db import models
 # from django.utils.timezone import now
@@ -7,11 +6,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Y a-t-il d’autres champs que vous souhaitez inclure pour la marque et le modèle de la voiture
-# - Méthode __str__ pour afficher un objet représentant la marque d’une voiture.
+
+# - Méthode __str__ pour afficher 
+# un objet représentant la marque d’une voiture.
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     marque = models.CharField(max_length=50)
@@ -22,19 +19,11 @@ class CarMake(models.Model):
         return self.name  # Return the name as the string representation
 
 
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -51,4 +40,5 @@ class CarModel(models.Model):
     # Other fields as needed
 
     def __str__(self):
-        return self.name  # Return the name as the string representation
+        # Return the name as the string representation
+        return self.name  
